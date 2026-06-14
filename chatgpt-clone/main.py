8f916cc7f68457a2cca43d890882d59b63296dc8
hiwon <stayby16@kakao.com>
2026-06-14 14:04:09 +0900

    🚀 [gpt] 260614_Streamlit_Data_Flow

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -1,38 +1,14 @@
 import streamlit as st
-import time
 
-st.write("Hello world")
+if "is_admin" not in st.session_state:
+    st.session_state["is_admin"] = False
 
-st.button("Click me Please")
+st.header("Hello!")
 
-st.text_input("Write your API KEY", max_chars=20)
+name  = st.text_input("What is your name?")
 
-st.feedback("faces")
+if name:
+    st.write(f"Hello {name}")
+    st.session_state["is_admin"] = True
 
-with st.sidebar:
-    # with -> 영역 구분, 이 아래 위젯들은 모두 sidebar 영역에 속함
-    st.badge("Badge 1")
-
-tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
-
-with tab1:
-    st.header("Agent")
-with tab2:
-    st.header("Agent2")
-with tab3:
-    st.header("Agent3")
-
-with st.chat_message("ai"):
-    st.text("Hello")
-    with st.status("Agent is using Tool") as status:
-        time.sleep(1)
-        status.update(label="Agent is searching the Web ...")
-        time.sleep(2)
-        status.update(label="Agent is reading the Page")
-        time.sleep(3)
-        status.update(state="complete")
-        
-with st.chat_message("human"):
-    st.text("Hi!")
-
-st.chat_input("Write a message for the assistant.", accept_file=True)
+print(st.session_state["is_admin"])
