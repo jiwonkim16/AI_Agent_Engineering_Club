14f94066f93f958c7c2ade42e94668a0ac2bc4e1
hiwon <stayby16@kakao.com>
2026-06-21 20:49:37 +0900

    🚀 [sup] 260621_Handoffs

--- customer-support-agent/my_agents/triage_agent.py
+++ customer-support-agent/my_agents/triage_agent.py
@@ -1,8 +1,29 @@
 # 유저의 입력을 받고 올바른 에이전트에게 전달
 # 주제와 관련된 질문인지를 확인하는 Guardrail 
 
-from agents import Agent, GuardrailFunctionOutput, RunContextWrapper, Runner, input_guardrail
-from models import InputGuardRailOutput, UserAccountContext
+import streamlit as st
+from agents import (
+    Agent,
+    RunContextWrapper,
+    input_guardrail,
+    Runner,
+    GuardrailFunctionOutput,
+    handoff,
+)
+from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
+from agents.extensions import handoff_filters
+from models import UserAccountContext, InputGuardRailOutput, HandoffData
+from my_agents.account_agent import account_agent
+from my_agents.technical_agent import technical_agent
+from my_agents.order_agent import order_agent
+from my_agents.billing_agent import billing_agent
+
+# Handoff(인계)
+# handoff가 처리되는 방식에는 크게 2가지 옵션이 있는데,
+# 1. handoff는 대화 자체가 다른 에이전트에게 넘어가야 함. (ex. A상담원과 대화 중 B 상담원으로 연결 전환, A 상담원과는 통신이 끊김)
+# 2. 다른 에이전트를 tool로서 처리할 수 있음. (ex. A 상담원과 대화 중 A 상담원이 다른 상담원에게 내 질문과 관련된 정보를 얻어 A 상담원이 나에게 답변을 주는 형태)
+
+# ---
 
 # 1.입력 가드레일(안전장치) 에이전트
 # 2.이 에이전트를 실제로 실행할 함수를 생성 (off_topic_guardrail 함수)
@@ -47,7 +68,10 @@ def dynamic_triage_agent_instructions(
     wrapper: RunContextWrapper[UserAccountContext],
     agent: Agent[UserAccountContext]
 ):
+    # RECOMMENDED_PROMPT_PREFIX는 OpenAI SDK에서 제공하는 프롬프트로, agent에 handoff가 있을 경우 agent 지침 최상단에 넣으라고 권장함.
     return f"""
+    {RECOMMENDED_PROMPT_PREFIX}
+
     You are a customer support agent. You ONLY help customers with their questions about their User Account, Billing, Orders, or Technical Support.
     You call customers by their name.
     
@@ -99,6 +123,36 @@ def dynamic_triage_agent_instructions(
     - Unclear issues: Ask 1-2 clarifying questions before routing
     """
 
+# context는 어디든 따라가기 때문에 여기서도 context를 가져올 수 있음.
+def handle_handoff(
+    wrapper: RunContextWrapper[UserAccountContext],
+    input_data: HandoffData,
+):
+
+    # handoff 가 발생하게된 이유 서술
+    with st.sidebar:
+        st.write(
+            f"""
+            Handing off to {input_data.to_agent_name}
+            Reason: {input_data.reason}
+            Issue Type: {input_data.issue_type}
+            Description: {input_data.issue_description}
+        """
+        )
+
+
+def make_handoff(agent):
+
+    return handoff(
+        agent=agent,
+        on_handoff=handle_handoff,
+        input_type=HandoffData, # AI 모델에게 이 handoff가 왜 발생했는지 설명???
+        input_filter=handoff_filters.remove_all_tools, # 새로운 에이전트가 볼 데이터를 골라서 넘길 수 있게 해주는 필터. (extension에서 import)
+        # 이 경우 에이전트가 호출했던 tool 사용 기록을 지워주고 유저와 에이전트 간 메세지만 남김.
+    )
+
+
+
 triage_agent = Agent(
     name="Triage_Agent",
      # instructions 시그니처를 보면 문자열을 넘길 수도 있고 RunContextWrapper와 에이전트를 매개변수로 받는 함수를 넘길 수도 있음.
@@ -106,5 +160,21 @@ triage_agent = Agent(
     instructions=dynamic_triage_agent_instructions,
     input_guardrails=[
         off_topic_guardrail
-    ] # 이렇게 해두면 triage_agent가 실행되기 전에 off_topic_guardrail 함수가 먼저 실행됨.
+    ], # 이렇게 해두면 triage_agent가 실행되기 전에 off_topic_guardrail 함수가 먼저 실행됨.
+
+    # 에이전트가 여러 개일 경우 일종의 tool로 사용 가능.
+    # tools=[
+    #     technical_agent.as_tool(
+    #         tool_name="Technical Help Tool",
+    #         tool_description="Use this when the user tech support"
+    #     )
+    # ]
+
+    # handoff를 import 해서 handoff가 일어날 때 호출되는 함수 등등 여러가지를 할 수 있음.
+    handoffs=[
+        make_handoff(technical_agent),
+        make_handoff(billing_agent),
+        make_handoff(account_agent),
+        make_handoff(order_agent),
+    ]
 )
