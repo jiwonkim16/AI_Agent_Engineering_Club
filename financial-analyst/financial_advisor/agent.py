b0915e3140c48beda6d33105df41af8a3afb5d13
hiwon <stayby16@kakao.com>
2026-06-24 22:08:59 +0900

    🚀 [finan] 260624_Agent_Architecture

--- financial-analyst/financial_advisor/agent.py
+++ financial-analyst/financial_advisor/agent.py
@@ -1,39 +1,38 @@
 # agent 생성 목적 파일
 # agent.py 파일에는 필수적으로 root_agent 라는 변수가 있어야 함.
-# 반드시 root_agent 여야 하는데, 이유는
+
+from cmd import PROMPT
 
 from google.adk.agents import Agent
 
 # Gemini 외에도 여러 LLM으로 작업할 수 있게 해주는 패키지
 from google.adk.models.lite_llm import LiteLlm
 
+# agent를 tool로 변환
+from google.adk.tools.agent_tool import AgentTool
+
+from financial_advisor.sub_agents import data_analyst, financial_analyst, news_analyst
+
 MODEL = LiteLlm("openai/gpt-4o")
 
 
-# tool (example)
-def get_weather(city: str):
-    return f"The weather in {city} is 30 degrees."
+def save_advice_report():
+    pass
 
-
-def convert_units(degrees: int):
-    return "That is 40 farenheit"
-
-
-geo_agent = Agent(
-    name="GeoAgent",
-    instruction="You help with geo questions",
-    model=MODEL,
-    description="Transfer to this agent when you have a geo related question.",
-)
 
 # Google ADK에서 Agent는 LlmAgent의 별칭
-agent = Agent(
-    name="WeatherAgent",
-    instruction="You help the user with weather related questions.",
+financial_advisor = Agent(
+    name="FinancialAdvisor",
+    instruction=PROMPT,
     model=MODEL,
-    tools=[get_weather, convert_units],
+    tools=[
+        AgentTool(agent=financial_analyst),
+        AgentTool(agent=news_analyst),
+        AgentTool(agent=data_analyst),
+        save_advice_report,
+    ],
     # OpenAI SDK의 Handoff와 같은 역할을 하는 것이 sub_agents
-    sub_agents=[geo_agent],
+    # sub_agents=[geo_agent],
 )
 
-root_agent = agent
+root_agent = financial_advisor
---
+++ financial-analyst/financial_advisor/sub_agents/news_analyst.py
@@ -0,0 +1,28 @@
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
+
+from tools import web_search_tool
+
+MODEL = LiteLlm(model="openai/gpt-4o")
+
+# firecrwal 대신 yfinance의 get_news API를 사용해도 됨. 이 방법이 비용을 절약할 수 있는 더 좋은 방법임. 공식문서 참고.
+news_analyst = Agent(
+    name="NewsAnalyst",
+    model=MODEL,
+    # root_agent가 볼 수 있도록, 이 에이전트가 언제 필요한지 판단할 수 있도록 역할/기준을 기술
+    description="Uses Web Search tools to search and scrape real web content from the web.",
+    instruction="""
+    You are a News Analyst Specialist who uses web tools to find current information. Your job:
+    
+    1. **Web Search**: Use web_search_tool() to find recent news about a company.
+    3. **Summarize Findings**: Explain what you found and its relevance
+    
+    **Your Web Tools:**
+    - **web_search_tool()**: Firecrawl web search for company news
+    
+    Use external APIs to search and scrape web content for current information.
+    """,
+    tools=[
+        web_search_tool,
+    ],
+)
